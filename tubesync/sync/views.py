be71cb1dafddfec1d80684e7ae3c3a72af14f65f
tcely <tcely@users.noreply.github.com>
2025-02-22 04:35:50 -0500

    Use the `members` class method

--- tubesync/sync/views.py
+++ tubesync/sync/views.py
@@ -903,7 +903,7 @@ class MediaServersView(ListView):
     def get_context_data(self, *args, **kwargs):
         data = super().get_context_data(*args, **kwargs)
         data['message'] = self.message
-        data['media_server_types'] = list(self.types_object.__members__.values())
+        data['media_server_types'] = self.types_object.members()
         return data
 
 
