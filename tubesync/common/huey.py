49b4a5e84bb3d821771f393ea41b7cf15cd57681
tcely <tcely@users.noreply.github.com>
2025-05-30 00:40:07 -0400

    Add `django-huey` managed tasks

---
+++ tubesync/common/huey.py
@@ -0,0 +1,30 @@
+
+
+def sqlite_tasks(key, /, prefix=None):
+    name_fmt = 'huey_{}'
+    if prefix is None:
+        prefix = ''
+    if prefix:
+        name_fmt = f'huey_{prefix}_' + '{}'
+    name = name_fmt.format(key)
+    return dict(
+        huey_class='huey.SqliteHuey',
+        name=name,
+        immediate=False,
+        results=True,
+        store_none=False,
+        utc=True,
+        compression=True,
+        connection=dict(
+            filename=f'/config/tasks/{name}.db',
+            fsync=True,
+            strict_fifo=True,
+        ),
+        consumer=dict(
+            workers=1,
+            worker_type='process',
+            max_delay=20.0,
+            scheduler_interval=10,
+        ),
+    )
+
